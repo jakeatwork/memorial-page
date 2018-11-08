96adcc748ab0bb9b0c3a9fb1487798eeb533e365
jakeatwork <jake@vote.org>
2018-11-08 10:02:06 -0500

    Fix python3 syntax issues

--- submissions/models.py
+++ submissions/models.py
@@ -41,7 +41,7 @@ class Link(models.Model):
         providers = micawber.bootstrap_basic()
         try:
             self.embed = providers.request(self.link)['html']
-        except micawber.ProviderException,e:
+        except micawber.ProviderException as e:
             self.embed = None
         super(Link,self).save(*args, **kwargs)
 
