cd4d045f737cdca2ece0415f0101e8da50a229ed
jakeatwork <jake@vote.org>
2018-11-09 10:25:22 -0500

    Another pattens fix

--- submissions/urls.py
+++ submissions/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import *
 
 from .views import submission, SubmissionListView, SubmissionUpdateView, ImageCreateView, delete_image
 from django.views.decorators.cache import cache_page
 
 
-urlpatterns = patterns(
+urlpatterns = [
     "",
     url(r"^$", cache_page(60*15)(SubmissionListView.as_view()), name='home'),
     url(r"^submit/", submission, name='submit'),
@@ -12,4 +12,4 @@ urlpatterns = patterns(
     url(r"^edit/(?P<pk>[0-9]+)/upload_image/$", ImageCreateView.as_view(), name='jfu-upload'),
     url(r"^edit/(?P<pk>[0-9]+)/delete_image/$", delete_image, name='jfu-delete'),
     url(r"^edit/(?P<pk>[0-9]+)/links/$", delete_image, name='link-inlines'),
-)
+]
